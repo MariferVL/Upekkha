21d2a80c95ae62bfe424a87d48db521fbfc90643
unknown <mfvl.1186@gmail.com>
2022-02-14 23:08:55 -0300

    abpro1 adherido

---
+++ Python/ABPro1/ABPro1.py
@@ -0,0 +1,18 @@
+"""Definir un listado de 5 productos con su respectivo valor unitario. (paltas: 30, tomates:50, churrascos: 45, mayonesa: 6, chucrut: 4)
+● Crear un archivo .py el cual deberá solicitar ingresar una cantidad por cada producto 
+(definido de la lista).
+● Se deberá mostrar en pantalla el total de la suma del pedido el que corresponde al valor neto.
+● Se deberá mostrar en pantalla el total del IVA (19%) del total de pedido ingresado.
+● Se deberá mostrar en pantalla el total final del pedido (la suma del valor neto + IVA)."""
+
+
+print("Ingresa cantidad de paltas")
+paltas= input()
+print("Ingresa cantidad de tomates")
+tomates= input()
+print("Ingresa cantidad de churrascos")
+churrascos= input()
+print("Ingresa cantidad de mayonesa")
+mayonesa= input()
+print("Ingresa cantidad de chucrut")
+chucrut= input()
