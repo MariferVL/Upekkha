adff10979bc3e3e496f8863739c07abad925dcf6
unknown <mfvl.1186@gmail.com>
2022-02-15 01:34:11 -0300

    updated

--- Python/ABPro1/ABPro1.py
+++ Python/ABPro1/ABPro1.py
@@ -5,28 +5,58 @@
 ● Se deberá mostrar en pantalla el total del IVA (19%) del total de pedido ingresado.
 ● Se deberá mostrar en pantalla el total final del pedido (la suma del valor neto + IVA)."""
 
-chorr= 22000 
+
+import inquirer
+questions = [
+    inquirer.List('products',
+                message="¿Qué producto desea comprar?",
+                choices=['chorrillana', 'chacarero', 'completo', 'terremoto', 'mojito'],
+            ),
+]
+answers = inquirer.prompt(questions)
+
+productos = {'chorrillana' : 22000,'chacarero': 7000,'completo': 6000,'terremoto':15000,'mojito': 20000}
+
+print("¿Cuánt@s {answers} desea?")
+cant= int(input())
+
+shares = input('How many shares? ')
+stock = input('What\'s the stock? ')
+
+for name in productos.keys():
+    precio = (productos[name])
+    if name == answers:
+        result = int(precio) * cant
+        percIVA= 19
+        iva= result * (percIVA / 100) 
+        precioplussIva= result + iva
+        print("Total: ${}".format(result))
+        print("IVA: ${}".format(iva))
+        print("Total con IVA: ${}".format(precioplussIva))
+
+
+"""chorr= 22000 
 chac= 7000 
 comp= 6000
 terr= 15000
-moj= 20000 
-
-print("¿Cuántas chorrillanas desea?")
-chorrCant= int(input())
-print("¿Cuántos chacareros desea?")
-chacCant= int(input())
-print("¿Cuántos completos desea?")
-compCant= int(input())
-print("¿Cuántos terremotos desea?")
-terrCant= int(input())
-print("¿Cuántos mojitos desea?")
-mojCant= int(input())
+moj= 20000 """
 
 
-result= chorr*chorrCant + chac*chacCant + comp*compCant + terr*terrCant +  moj*mojCant
+# print("¿Cuántas chorrillanas desea?")
+# chorrCant= int(input())
+# print("¿Cuántos chacareros desea?")
+# chacCant= int(input())
+# print("¿Cuántos completos desea?")
+# compCant= int(input())
+# print("¿Cuántos terremotos desea?")
+# terrCant= int(input())
+# print("¿Cuántos mojitos desea?")
+# mojCant= int(input())
+
+"""result= chorr*chorrCant + chac*chacCant + comp*compCant + terr*terrCant +  moj*mojCant
 percIVA= 19
 iva= result * (percIVA / 100) 
 precioplussIva= result + iva
 print("Total: ${}".format(result))
 print("IVA: ${}".format(iva))
-print("Total con IVA: ${}".format(precioplussIva))
+print("Total con IVA: ${}".format(precioplussIva))"""
